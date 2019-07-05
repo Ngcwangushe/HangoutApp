2bce84c19229def8409a43c941580cd384f2bead
Sandiso Ngcwangushe <sbngcwangushe@gmail.com>
2019-07-05 10:05:37 +0200

    -Add angular routin -Using routerLinkActive -protecting the roots using rootguard

--- HangoutApp-SPA/src/app/nav/nav.component.ts
+++ HangoutApp-SPA/src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -10,16 +11,19 @@ import { AlertifyService } from '../_services/alertify.service';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(public authService: AuthService, private aletify: AlertifyService) { }
+  constructor(public authService: AuthService, private aletify:
+     AlertifyService, private router: Router) { }
 
   ngOnInit() {
   }
 
   login() {
     this.authService.login(this.model).subscribe(next => {
-      this.aletify.success('Logged in successfully');
-    }, error => {
+      this.aletify.success('Logged in successfully'); // next
+    }, error => { // Error
       (this.aletify.error(error));
+    }, () => {
+      this.router.navigate(['/members']); // Complete
     });
   }
 
@@ -29,5 +33,6 @@ export class NavComponent implements OnInit {
   logout() {
     localStorage.removeItem('token');
     this.aletify.message('logged out');
+    this.router.navigate(['/home']);
   }
 }
